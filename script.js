90c434d0c3ae1e02ec7b5028a8553552f317ad4d
Yiddies <chrislicks104@gmail.com>
2023-01-20 21:56:38 -0500

    scripting

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function getComputerChoice() {
-    return math.random()
+let rpsValue = 0
+
+function getRandomInt(min, max) {
+    rpsValue = Math.floor(Math.random() * (max -min + 1) + min)
+    return rpsValue
 }
 
+function getComputerChoice() {
+    if(rpsValue == 1) {
+        return("Rock");
+    } else if(rpsValue == 2) {
+        return("Paper");
+    } else {
+        return("Scissor");
+    }
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Please enter a move");
+
+}
+
+function rpsGame(playerSelection, computerSelection) {
+}
+
+getPlayerChoice();
+rpsGame(getPlayerChoice, getComputerChoice);
+getRandomInt(1, 3);
 getComputerChoice();
+ console.log(getPlayerChoice)
